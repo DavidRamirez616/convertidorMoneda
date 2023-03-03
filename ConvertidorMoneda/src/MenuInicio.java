494dee2fa6d464ab5230f007b89c291e25bcba90
David Ramirez <123591054+DavidRamirez616@users.noreply.github.com>
2023-03-02 22:53:31 -0600

    Update MenuInicio.java
    especificando las unidades de masa

--- ConvertidorMoneda/src/MenuInicio.java
+++ ConvertidorMoneda/src/MenuInicio.java
@@ -4,7 +4,7 @@ public class MenuInicio {
 	public static void main(String[] args) {
 
 		String opcion = (String) JOptionPane.showInputDialog(null, "Seleccione una opcion", "MENU",
-				JOptionPane.PLAIN_MESSAGE, null, new Object[] { "Convertidor de Divisa", "Convertidor de Unidades" },
+				JOptionPane.PLAIN_MESSAGE, null, new Object[] { "Convertidor de Divisa", "Convertidor de Unidades Masa" },
 				null);
 
 		switch (opcion) {
